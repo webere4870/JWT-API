6fde7ef4e335af3325ef5837fceadee17ffb063b
Eli Weber <eliweber2001@gmail.com>
2022-04-30 13:12:11 -0400

    ENV change

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 require('dotenv').config({path: __dirname + '/.env'})
-mongoose.createConnection(process.env.DB_STRING, {
+mongoose.createConnection("mongodb+srv://webere4870:Jack$Bauer$24$@cluster0.fyc6b.mongodb.net/Users?retryWrites=true&w=majority", {
     useUnifiedTopology:true,
     useNewUrlParser: true,
 });
